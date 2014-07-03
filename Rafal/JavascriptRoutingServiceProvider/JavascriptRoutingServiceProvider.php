3a0b686dd2651a3fa98224cf5711922335891513
Alessandro Paterno <alexptrn@gmail.com>
2014-07-03 18:06:49 +0200

    Add compatibility with PHP 5.3

--- Rafal/JavascriptRoutingServiceProvider/JavascriptRoutingServiceProvider.php
+++ Rafal/JavascriptRoutingServiceProvider/JavascriptRoutingServiceProvider.php
@@ -15,30 +15,49 @@ class JavascriptRoutingServiceProvider implements ServiceProviderInterface
     public function boot(Application $app)
     {
         $app->before(function(Request $request) use($app) {
-            if (!isset($app['jsrouting.path'])) {
-                throw new \Exception('Missing `jsrouting.path` option!');
+            if (!isset($app["jsrouting.path"])) {
+                throw new \Exception("Missing `jsrouting.path` option!");
             }
-            $path = $app['jsrouting.path'];
-            $file_name = isset($app['jsrouting.file_neme']) ? $app['jsrouting.path'] : 'router.js';
-            $refresh = isset($app['jsrouting.refresh']) ? (bool)$app['jsrouting.refresh'] : true;
-            $add_basepath = isset($app['jsrouting.basepath']) ? (bool)$app['jsrouting.basepath'] : true;
+            $path = $app["jsrouting.path"];
+            $file_name = isset($app["jsrouting.file_name"]) ? $app["jsrouting.file_name"] : "router.js";
+            $refresh = isset($app["jsrouting.refresh"]) ? (bool)$app["jsrouting.refresh"] : true;
+            $add_basepath = isset($app["jsrouting.basepath"]) ? (bool)$app["jsrouting.basepath"] : true;
+            $skip = array();
+            if (isset($app["jsrouting.skip"])) {
+                if (is_array($app["jsrouting.skip"])) {
+                    $skip = $app["jsrouting.skip"];
+                } else {
+                    $skip = array($app["jsrouting.skip"]);
+                }
+            }
 
             if ($refresh === true) {
-                if (!file_exists($path . '/' . $file_name)) {
-                    touch($path . '/' . $file_name);
+                if (!file_exists($path . "/" . $file_name)) {
+                    touch($path . "/" . $file_name);
                 }
                 $routes = array();
-                foreach ($app['routes']->all() as $name => $route) {
+                foreach ($app["routes"]->all() as $name => $route) {
+                    $pattern = $route->getPattern();
+                    $continue = false;
+                    foreach ($skip as $p) {
+                        if (preg_match($p, $pattern)) {
+                            $continue = true;
+                            break;
+                        }
+                    }
+                    if ($continue) {
+                        continue;
+                    }
                     $routes[$name] = array(
-                        'pattern'       => $route->getPattern(),
-                        'requirements'  => $route->getRequirements(),
-                        'defaults'      => $route->getDefaults(),
-                        'variables'     => $route->compile()->getVariables()
+                        "pattern"       => $pattern,
+                        "requirements"  => $route->getRequirements(),
+                        "defaults"      => $route->getDefaults(),
+                        "variables"     => $route->compile()->getVariables()
                     );
-                    unset($routes[$name]['requirements']['_method']);
+                    unset($routes[$name]["requirements"]["_method"], $routes[$name]["defaults"]["_controller"]);
                 }
                 
-                file_put_contents($path . '/' . $file_name, $this->jsContent(json_encode($routes), json_encode($add_basepath), $request->getBasePath()));
+                file_put_contents($path . "/" . $file_name, $app['jsrouting']->jsContent(json_encode($routes), json_encode($add_basepath), $request->getBasePath()));
             }
         });
     }
@@ -47,7 +66,7 @@ class JavascriptRoutingServiceProvider implements ServiceProviderInterface
         return <<<JS
 var Router = {
     routes: {$data},
-    basepath: '{$basePath}',
+    basepath: "{$basePath}",
     add_basepath: {$add_basepath},
 
     get: function(name, params, add_basepath) {
@@ -66,16 +85,16 @@ var Router = {
                 param = variables[param];
                 val = params[param] != undefined ? params[param] : defaults[param];
                 if (val == undefined) {
-                    throw 'Missing "'+param+'" parameter for route "'+name+'"!';
+                    throw "Missing '"+param+"' parameter for route '"+name+"'!";
                 }
                 if (requirements[param] && !new RegExp(requirements[param]).test(val)) {
-                    throw 'Parameter "'+param+'" for route "'+name+'" must pass "'+requirements[param]+'" test!';
+                    throw "Parameter '"+param+"' for route '"+name+"' must pass '"+requirements[param]+"' test!";
                 }
-                result = result.replace('{'+param+'}', val);
+                result = result.replace("{"+param+"}", val);
             }
-            return (add_basepath ? this.basepath : '') + result;
+            return (add_basepath ? this.basepath : "") + result;
         } else {
-            throw 'Undefined route "'+name+'"!';
+            throw "Undefined route '"+name+"'!";
         }
     }
 }
